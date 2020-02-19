fd8a37f44c51f492183b0912f1ff72fdad8a4cb9
light130 <light13silver@gmail.com>
2020-02-19 22:52:18 +0900

    ユーザー作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,9 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
+  validates :name, presence: true, length: { maximum: 30 }
+
+  private
+    def set_nameless_name
+      self.name = '名前なし' if name.blank?
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_043636) do
+ActiveRecord::Schema.define(version: 2020_02_19_103308) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
