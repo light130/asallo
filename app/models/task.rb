8f2c0770821140d51b662b9aa133df628a629a16
light130 <light13silver@gmail.com>
2020-02-21 23:04:46 +0900

    Add ransack

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,15 @@
 class Task < ApplicationRecord
-  before_validation :set_nameless_name
   validates :name, presence: true, length: { maximum: 30 }
 
   belongs_to :user
 
   scope :recent, -> { order(created_at: :desc) }
 
-  private
-    def set_nameless_name
-      self.name = '名前なし' if name.blank?
-    end
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+  
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
