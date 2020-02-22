05dc6761d9e96f4858d560f480e13b958005fd78
light130 <light13silver@gmail.com>
2020-02-22 23:20:06 +0900

    CSVデータを扱う

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
@@ -7,6 +10,6 @@ Rails.application.routes.draw do
   end
   root to: 'tasks#index'
   resources :tasks do
-    post :confirm, action: :confirm_new, on: :new
+    post :import, on: :collection
   end
 end
